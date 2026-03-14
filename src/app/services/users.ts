d2c15b67b7a9aff04c668497733e5c8e97c9a257
Adrián Amat Bernabéu <adrianamatbernabeu@gmail.com>
2026-03-14 12:22:49 +0100

    Creamos el método getAll para pedir los datos a la api en el users.servicio.ts

--- src/app/services/users.ts
+++ src/app/services/users.ts
@@ -1,8 +1,21 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { inject } from '@angular/core';
+import { IUser, IUserResponse } from '../interfaces/iuser.interface';
+import { lastValueFrom } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class Users {
+  private httpClient = inject(HttpClient);
+  private baseUrl = 'https://peticiones.online/api/users'; 
+
   
+  async getAll(): Promise <IUserResponse> {
+  // Convertimos el Observable a Promesa y pedimos los datos
+  return await lastValueFrom(this.httpClient.get<IUserResponse>(this.baseUrl));
+}
+
+
 }
