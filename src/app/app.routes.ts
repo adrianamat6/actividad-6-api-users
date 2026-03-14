c3af369bf26d27db16d3c88f49472a16ee6bb8d1
Adrián Amat Bernabéu <adrianamatbernabeu@gmail.com>
2026-03-14 11:11:31 +0100

    Creamos el sistema de rutas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { UserViewComponent } from './pages/user-view/user-view.component';
+import { UserFormComponent } from './pages/user-form/user-form.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: 'home', pathMatch: 'full' },
+    { path: 'home', component: HomeComponent },
+    { path: 'user/:id', component: UserViewComponent},   // Ruta dinámica para detalle
+    { path: 'newuser', component: UserFormComponent},    // Ruta para crear
+    { path: 'updateuser/:id', component: UserFormComponent},  // Ruta para editar
+    { path: '**', redirectTo: 'home' } // Comodín para errores 404
+];
