b54be858fa4fdf1482afc9469fdf3dc9e7afc67d
Adrián Amat Bernabéu <adrianamatbernabeu@gmail.com>
2026-03-19 10:19:07 +0100

    Empezamos con el desarrollo del formulario en user-form.html y user-form.ts

--- src/app/pages/user-form/user-form.component.ts
+++ src/app/pages/user-form/user-form.component.ts
@@ -1,11 +1,36 @@
 import { Component } from '@angular/core';
+import { FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { RouterLink } from '@angular/router';
+import { Validators } from '@angular/forms';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-user-form',
-  imports: [],
+  imports: [ReactiveFormsModule,RouterLink],
   templateUrl: './user-form.component.html',
   styleUrl: './user-form.component.css',
 })
 export class UserFormComponent {
 
+  userForm: FormGroup; 
+
+  emailPattern: string = '^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$';
+  constructor(){
+    this.userForm = new FormGroup( {
+      first_name: new FormControl('', [Validators.required, Validators.minLength(3)] ), 
+      last_name: new FormControl('', [Validators.required, Validators.minLength(3)] ), 
+      username: new FormControl('',[Validators.required]), 
+      email: new FormControl('',[Validators.required, Validators.pattern(this.emailPattern)]),
+      image: new FormControl('', [Validators.required]) 
+    })
+  }
+
+    checkError(controlName: string, errorName: string): boolean | undefined {
+      return this.userForm.get(controlName)?.hasError(errorName) && this.userForm.get(controlName)?.touched;
+  }
+
+  addUser() {
+    console.log("Valores del formulario:", this.userForm.value);
+  }
+  
 }
